1e627beb1e87543ffb3a2b46078862d8fbfccaff
Alexander Sapyanov <work.sapyanov@gmail.com>
2021-03-04 08:24:52 +0400

    Initial commit

---
+++ PryanikyTestWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PryanikyTestWork
+//
+//  Created by Alexander  Sapianov on 04.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
